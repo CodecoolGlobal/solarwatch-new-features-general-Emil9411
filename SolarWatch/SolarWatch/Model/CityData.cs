86f816b759021bad8c6f685eb74f0479bc2a33b3
Emil9411 <almadi.emil94@gmail.com>
2024-03-28 22:00:50 +0100

    Created CityData model, for handling city's latitude, longitude and name.

---
+++ SolarWatch/SolarWatch/Model/CityData.cs
@@ -0,0 +1,8 @@
+namespace SolarWatch.Model;
+
+public class CityData
+{
+    public string? City { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
