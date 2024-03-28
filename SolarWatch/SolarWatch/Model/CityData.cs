d4b6bf8a8d0e77e6b69ef46774daf2d70c9bf0ae
Emil9411 <almadi.emil94@gmail.com>
2024-03-28 23:56:25 +0100

    CityData model extended with necessary primary key.

--- SolarWatch/SolarWatch/Model/CityData.cs
+++ SolarWatch/SolarWatch/Model/CityData.cs
@@ -2,6 +2,7 @@ namespace SolarWatch.Model;
 
 public class CityData
 {
+    public int Id { get; set; }
     public string? City { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
