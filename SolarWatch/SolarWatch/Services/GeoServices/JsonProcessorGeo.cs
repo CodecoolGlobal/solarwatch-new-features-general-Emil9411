5bc06efc0e2a71939f24bea8f736f8401e830951
Emil9411 <almadi.emil94@gmail.com>
2024-03-28 22:02:14 +0100

    JsonProcessorGeo now using the new city data model.

--- SolarWatch/SolarWatch/Services/GeoServices/JsonProcessorGeo.cs
+++ SolarWatch/SolarWatch/Services/GeoServices/JsonProcessorGeo.cs
@@ -1,15 +1,22 @@
 using System.Text.Json;
+using SolarWatch.Model;
 
 namespace SolarWatch.Services.GeoServices;
 
 public class JsonProcessorGeo : IJsonProcessorGeo
 {
-    public double[] LongLatProcessor(string data)
+    public CityData LongLatProcessor(string data)
     {
         var json = JsonDocument.Parse(data);
         var results = json.RootElement[0];
         var lat = results.GetProperty("lat").GetDouble();
         var lon = results.GetProperty("lon").GetDouble();
-        return new double[] {lat, lon};
+        var city = results.GetProperty("name").GetString();
+        return new CityData
+        {
+            City = city,
+            Latitude = lat,
+            Longitude = lon
+        };
     }
 }
