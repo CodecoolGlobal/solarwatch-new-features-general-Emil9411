976b0132746ed0120a68ae060e040dfe8e52cd83
Emil9411 <almadi.emil94@gmail.com>
2024-03-28 21:13:30 +0100

    Added the necessary services, small refactor.

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,11 +1,12 @@
+using SolarWatch.Services.GeoServices;
+using SolarWatch.Services.SWServices;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+AddServices();
+ConfigureSwagger();
+// AddDbContext();
+// AddAuthentication();
+// AddIdentity();
 
 var app = builder.Build();
 
@@ -16,10 +17,28 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+return;
+
+void AddServices()
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddControllers(
+        options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+    builder.Services.AddSingleton<IGeoApi, GeoApi>();
+    builder.Services.AddSingleton<IJsonProcessorGeo, JsonProcessorGeo>();
+    builder.Services.AddSingleton<ISWApi, SWApi>();
+    builder.Services.AddSingleton<IJsonProcessorSW, JsonProcessorSW>();
+}
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen();
+}
