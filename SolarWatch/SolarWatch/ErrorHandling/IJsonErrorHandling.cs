8068852ed312c775bf5e35c69169301b60005225
Emil9411 <almadi.emil94@gmail.com>
2024-03-29 23:47:01 +0100

    Interface for the json error handler class implemented.

---
+++ SolarWatch/SolarWatch/ErrorHandling/IJsonErrorHandling.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolarWatch.ErrorHandling;
+
+public interface IJsonErrorHandling
+{
+    ActionResult SolarJsonError(string solarJson);
+    ActionResult GeoJsonError(string geoJson);
+}
