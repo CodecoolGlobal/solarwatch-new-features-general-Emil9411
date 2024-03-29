de75221ef8035dfabf521c537b3893efb57250fd
Emil9411 <almadi.emil94@gmail.com>
2024-03-29 23:45:03 +0100

    SolarWatch data model got extended with an Equals, and GetHashCode override.

--- SolarWatch/SolarWatch/Model/SWData.cs
+++ SolarWatch/SolarWatch/Model/SWData.cs
@@ -7,4 +7,20 @@ public class SWData
     public DateOnly Date { get; set; }
     public TimeOnly Sunrise { get; set; }
     public TimeOnly Sunset { get; set; }
+    
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var swData = (SWData)obj;
+        return City == swData.City && Date == swData.Date && Sunrise == swData.Sunrise && Sunset == swData.Sunset;
+    }
+    
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(City, Date, Sunrise, Sunset);
+    }
 }
