6487e61c750c2433d948df10a4349676384b4508
Emil9411 <almadi.emil94@gmail.com>
2024-03-30 12:15:49 +0100

    Necessary services registered, swagger configured, authentication with cookies added.

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,7 +1,12 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SolarWatch.Data;
 using SolarWatch.ErrorHandling;
+using SolarWatch.Model;
 using SolarWatch.Services.Auth;
 using SolarWatch.Services.GeoServices;
 using SolarWatch.Services.SWServices;
@@ -12,10 +17,16 @@ var builder = WebApplication.CreateBuilder(args);
 AddServices();
 ConfigureSwagger();
 AddDbContext();
-// AddAuthentication();
+AddAuthentication();
 AddIdentity();
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var authenticationSeeder = scope.ServiceProvider.GetRequiredService<AuthSeeder>();
+authenticationSeeder.AddRoles();
+authenticationSeeder.AddAdmin();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -42,6 +53,8 @@ void AddServices()
     builder.Services.AddScoped<IGeoRepository, GeoRepository>();
     builder.Services.AddScoped<ISWRepository, SWRepository>();
     builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+    builder.Services.AddScoped<AuthSeeder>();
     builder.Services.AddSingleton<INormalizeCityName, NormalizeCityName>();
     builder.Services.AddSingleton<SpecialCharReplacements>();
     builder.Services.AddSingleton<IJsonErrorHandling, JsonErrorHandling>();
@@ -54,7 +67,33 @@ void AddServices()
 
 void ConfigureSwagger()
 {
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
+    });
 }
 
 void AddDbContext()
@@ -72,7 +111,7 @@ void AddDbContext()
 void AddIdentity()
 {
     builder.Services
-        .AddIdentityCore<IdentityUser>(options =>
+        .AddIdentityCore<ApplicationUser>(options =>
         {
             options.SignIn.RequireConfirmedAccount = false;
             options.User.RequireUniqueEmail = true;
@@ -82,5 +121,42 @@ void AddIdentity()
             options.Password.RequireUppercase = false;
             options.Password.RequireLowercase = false;
         })
+        .AddRoles<IdentityRole>()
         .AddEntityFrameworkStores<UsersContext>();
+}
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddCookie(options => {
+            options.Cookie.Name = "Authorization";
+        })
+        .AddJwtBearer(options =>
+        {
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+            var issuerSigningKey = builder.Configuration.GetSection("IssuerSigningKey").Value;
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.ValidIssuer,
+                ValidAudience = jwtSettings.ValidAudience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(issuerSigningKey)),
+            };
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    if (context.Request.Cookies.ContainsKey("Authorization"))
+                    {
+                        context.Token = context.Request.Cookies["Authorization"];
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        });
 }
