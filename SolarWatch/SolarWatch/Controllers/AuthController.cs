964524ea7d57f224e3e993465455ef1a00cf26e7
Emil9411 <almadi.emil94@gmail.com>
2024-03-30 12:09:26 +0100

    AuthController extended with login, logout, and verifying endpoints.

--- SolarWatch/SolarWatch/Controllers/AuthController.cs
+++ SolarWatch/SolarWatch/Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SolarWatch.Contracts;
 using SolarWatch.Services.Auth;
@@ -23,7 +24,7 @@ public class AuthController : ControllerBase
             return BadRequest(ModelState);
         }
         
-        var result = await _authService.RegisterAsync(request.Email, request.Username, request.Password);
+        var result = await _authService.RegisterAsync(request.Email, request.Username, request.Password, request.City, "User");
         
         if (!result.Success)
         {
@@ -32,6 +33,54 @@ public class AuthController : ControllerBase
         }
         
         return CreatedAtAction(nameof(Register), new RegistrationResponse (result.Email, result.UserName ));
+    }
+    
+    [HttpPost("login")]
+    public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        var result = await _authService.LoginAsync(request.EmailOrUserName, request.Password);
+        
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+        
+        Response.Cookies.Append("Authorization", result.Token, new CookieOptions
+        {
+            HttpOnly = true
+        });
+        
+        return Ok(new AuthResponse(result.Email, result.UserName));
+    }
+    
+    [HttpGet("WhoAmI"), Authorize(Roles = "User,Admin")]
+    public ActionResult<AuthResponse> WhoAmI()
+    {
+        var cookieString = Request.Cookies["Authorization"];
+        
+        var token = _authService.Verify(cookieString);
+        
+        if (token != null)
+        {
+            var claims = token.Claims;
+            var email = claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+            var username = claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
+            return Ok(new AuthResponse(email, username));
+        }
+        return BadRequest("No token found");
+    }
+
+    [HttpPost("Logout"), Authorize(Roles = "User,Admin")]
+    public ActionResult Logout()
+    {
+        Response.Cookies.Delete("Authorization");
+        return Ok();
     }
     
     private void AddErrors(AuthResult result)
