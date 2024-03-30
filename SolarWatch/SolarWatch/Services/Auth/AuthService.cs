65d7b6f98cbc7a20f278b5dc628b0cf80b31b7f6
Emil9411 <almadi.emil94@gmail.com>
2024-03-30 11:45:28 +0100

    AuthService class has been extended with login and verify functionalities.

--- SolarWatch/SolarWatch/Services/Auth/AuthService.cs
+++ SolarWatch/SolarWatch/Services/Auth/AuthService.cs
@@ -1,37 +1,91 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using SolarWatch.Model;
 
 namespace SolarWatch.Services.Auth;
 
 public class AuthService : IAuthService
 {
-    private readonly UserManager<IdentityUser> _userManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ITokenService _tokenService;
+    private readonly IConfiguration _configuration;
     
-    public AuthService(UserManager<IdentityUser> userManager)
+    public AuthService(UserManager<ApplicationUser> userManager, ITokenService tokenService, IConfiguration configuration)
     {
         _userManager = userManager;
+        _tokenService = tokenService;
+        _configuration = configuration;
     }
     
-    public async Task<AuthResult> RegisterAsync(string email, string username, string password)
+    public async Task<AuthResult> RegisterAsync(string email, string username, string password, string city, string role)
     {
-        var existingUser = await _userManager.FindByEmailAsync(email);
-        if (existingUser != null)
-        {
-            return new AuthResult(false, email, username, null)
-            {
-                ErrorMessages = { { "email", "Email already in use" } }
-            };
-        }
-        
-        var newUser = new IdentityUser
+        var user = new ApplicationUser
         {
             Email = email,
-            UserName = username
+            UserName = username,
+            City = city
         };
+        var result = await _userManager.CreateAsync(user, password);
         
-        var createdUser = await _userManager.CreateAsync(newUser, password);
-        return !createdUser.Succeeded ? FailedRegistration(createdUser, email, username) : new AuthResult(true, email, username, null);
+        if (!result.Succeeded)
+        {
+            return FailedRegistration(result, email, username);
+        }
+        
+        await _userManager.AddToRoleAsync(user, role);
+        return new AuthResult(true, email, username, "");
+    }
+
+    public async Task<AuthResult> LoginAsync(string emailOrUserName, string password)
+    {
+        if (IsValidEmail(emailOrUserName))
+        {
+            var user = await _userManager.FindByEmailAsync(emailOrUserName);
+            if (user == null)
+            {
+                return InvalidUsername(emailOrUserName);
+            }
+            var result = await _userManager.CheckPasswordAsync(user, password);
+            if (!result)
+            {
+                return InvalidPassword(emailOrUserName, user.UserName);
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            var accessToken = _tokenService.CreateToken(user, roles[0]);
+            return new AuthResult(true, emailOrUserName, user.UserName, accessToken);
+        }
+        else
+        {
+            var user = await _userManager.FindByNameAsync(emailOrUserName);
+            if (user == null)
+            {
+                return InvalidUsername(emailOrUserName);
+            }
+            var result = await _userManager.CheckPasswordAsync(user, password);
+            if (!result)
+            {
+                return InvalidPassword(user.Email, emailOrUserName);
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            var accessToken = _tokenService.CreateToken(user, roles[0]);
+            return new AuthResult(true, user.Email, emailOrUserName, accessToken);
+        }
     }
     
+    public JwtSecurityToken Verify(string token){
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(_configuration.GetSection("IssuerSigningKey").Value);
+        tokenHandler.ValidateToken(token, new TokenValidationParameters{
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        }, out SecurityToken validatedToken);
+        return (JwtSecurityToken)validatedToken;
+    }
+
     private static AuthResult FailedRegistration(IdentityResult result, string email, string username)
     {
         var authResult = new AuthResult(false, email, username, "");
@@ -42,6 +96,33 @@ public class AuthService : IAuthService
         }
 
         return authResult;
+    }
+    
+    private bool IsValidEmail(string email)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(email);
+            return addr.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
+    private static AuthResult InvalidUsername(string username)
+    {
+        var result = new AuthResult(false, "", username, "");
+        result.ErrorMessages.Add("Bad credentials", "Invalid username");
+        return result;
+    }
+
+    private static AuthResult InvalidPassword(string email, string userName)
+    {
+        var result = new AuthResult(false, email, userName, "");
+        result.ErrorMessages.Add("Bad credentials", "Invalid password");
+        return result;
     }
     
 }
