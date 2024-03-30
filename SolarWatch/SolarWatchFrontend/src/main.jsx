ef5d9e92652ae8ed8f810335c696ca8a8c43fc66
Emil9411 <almadi.emil94@gmail.com>
2024-03-30 23:58:41 +0100

    Updated main.jsx, now it uses client side routing.

--- SolarWatch/SolarWatchFrontend/src/main.jsx
+++ SolarWatch/SolarWatchFrontend/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+import Login from './pages/Login.jsx'
+import Registration from './pages/Registration.jsx'
+import SolarWatch from './pages/SolarWatch.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/login', element: <Login /> },
+      { path: '/register', element: <Registration /> },
+      { path: '/solarwatch', element: <SolarWatch /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
