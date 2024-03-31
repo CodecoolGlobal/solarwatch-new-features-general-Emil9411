0184e40c069b4fbb10439fe268bc17320f3262f0
Emil9411 <almadi.emil94@gmail.com>
2024-04-01 00:32:04 +0200

    New table to show SolarWatch data for the user created.

---
+++ SolarWatch/SolarWatchFrontend/src/components/OneRowTable.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../design/oneRowTable.css";
+import { capitalizeWords } from "../utilities/utils";
+
+function OneRowTable({ dataObject }) {
+    const filteredKeys = Object.keys(dataObject).filter(key => key !== "id");
+    return (
+        <table>
+        <tbody>
+            {filteredKeys.map((key) => (
+            <tr key={key}>
+                <td>{capitalizeWords(key)}</td>
+                <td>{dataObject[key]}</td>
+            </tr>
+            ))}
+            <tr>
+                <td><button>Save City</button></td>
+                <td><button>Save City & Date</button></td>
+            </tr>
+        </tbody>
+        </table>
+    );
+}
+
+export default OneRowTable;
