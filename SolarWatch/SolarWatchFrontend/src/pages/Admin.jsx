9cfe0f55b42fd9a92f1071fc2ca1bee6aba840f4
Emil9411 <almadi.emil94@gmail.com>
2024-04-01 15:08:20 +0200

    introduced useEffect for fetching the data from the database, new delete and update functions, and extended conditional rendering with the update form.

--- SolarWatch/SolarWatchFrontend/src/pages/Admin.jsx
+++ SolarWatch/SolarWatchFrontend/src/pages/Admin.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../design/index.css";
 import AdminTable from "../components/AdminTable";
+import AdminUpdateForm from "../components/AdminUpdateForm";
 
 function Admin() {
-  const [data, setData] = useState(null);
-  const [loaded, setLoaded] = useState(false);
+  const [cityData, setCityData] = useState([]);
+  const [cityToUpdate, setCityToUpdate] = useState({});
+  const [updateFrom, setUpdateFrom] = useState(false);
 
-  async function fetchData() {
-    try {
+  useEffect(() => {
+    async function fetchData() {
       const response = await fetch(`/api/SW/getall`, {
         method: "GET",
         headers: {
@@ -16,22 +18,66 @@ function Admin() {
         },
       });
       const data = await response.json();
-      console.log(data);
-      setData(data);
-      setLoaded(true);
+      setCityData(data);
+    }
+    fetchData();
+  }, []);
+
+  async function handleDelete(id) {
+    try {
+      await fetch(`/api/SW/delete/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setCityData(cityData.filter((row) => row.id !== id));
     } catch (e) {
       console.error(e);
     }
+  }
+
+  async function handleUpdate(id, data) {
+    try {
+      await fetch(`/api/SW/update/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      setCityData(
+        cityData.map((row) => (row.id === id ? { ...row, ...data } : row))
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  function handleDataToUpdate(data) {
+    setCityToUpdate(data);
+    setUpdateFrom(true);
+  }
+
+  function clearData() {
+    setCityToUpdate({});
+    setUpdateFrom(false);
   }
 
   return (
     <div>
       <h1>Admin site</h1>
       <br />
-      {loaded === true ? (
-        <AdminTable data={data} />
+      {cityData.length !== 0 && updateFrom === false ? (
+        <AdminTable data={cityData} onDelete={handleDelete} onUpdate={handleDataToUpdate} />
+      ) : updateFrom === true ? (
+        <AdminUpdateForm
+          data={cityToUpdate}
+          onUpdate={handleUpdate}
+          clearData={clearData}
+        />
       ) : (
-        <button onClick={fetchData}>Get All SolarWatch Data</button>
+        <h2>No data available</h2>
       )}
     </div>
   );
