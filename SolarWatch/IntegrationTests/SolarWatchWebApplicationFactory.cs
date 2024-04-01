a00974462ec1415f79a0316e6cd21c7f86676820
Emil9411 <almadi.emil94@gmail.com>
2024-04-02 00:47:14 +0200

    Created the WebApplicationFactory.

---
+++ SolarWatch/IntegrationTests/SolarWatchWebApplicationFactory.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using SolarWatch.Data;
+
+namespace IntegrationTests;
+
+internal class SolarWatchWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            var env = context.HostingEnvironment;
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+        });
+        
+        builder.ConfigureServices((context, services) =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<DataContext>));
+            services.RemoveAll(typeof(DbContextOptions<UsersContext>));
+            
+            var configuration = context.Configuration;
+            var connectionString = configuration.GetConnectionString("TestDatabaseConnection");
+            
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(connectionString));
+            
+            services.AddDbContext<UsersContext>(options =>
+                options.UseSqlServer(connectionString));
+            
+            services.AddAuthentication("TestUserScheme")
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("TestUserScheme", options => { });
+            
+            services.AddAuthentication("TestAdminScheme")
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("TestAdminScheme", options => { });
+
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+            var usersContext = scope.ServiceProvider.GetRequiredService<UsersContext>();
+                
+            if(!dataContext.Database.CanConnect() || !usersContext.Database.CanConnect())
+            {
+                dataContext.Database.EnsureCreated();
+                usersContext.Database.Migrate();
+                usersContext.Database.EnsureCreated();
+            }
+            else
+            {
+                dataContext.Database.EnsureDeleted();
+                usersContext.Database.EnsureDeleted();
+                dataContext.Database.EnsureCreated();
+                usersContext.Database.Migrate();
+                usersContext.Database.EnsureCreated();
+            }
+        });
+    }
+}
