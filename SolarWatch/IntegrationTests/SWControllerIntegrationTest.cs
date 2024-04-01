ceef005c9c5d889259e873e080db4b14da399ef5
Emil9411 <almadi.emil94@gmail.com>
2024-04-02 00:48:05 +0200

    SolarWatch controller endpoints tested.

---
+++ SolarWatch/IntegrationTests/SWControllerIntegrationTest.cs
@@ -0,0 +1,85 @@
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using SolarWatch.Model;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IntegrationTests;
+
+public class SWControllerIntegrationTest
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    
+    public SWControllerIntegrationTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    [Fact]
+    public async Task GetData_UserAuthorized_Test()
+    {
+        var app = new SolarWatchWebApplicationFactory();
+        var client = app.CreateClient();
+        
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("TestUserScheme");
+        
+        var response = await client.GetAsync($"/api/SW/getdata/Stockholm/2022-01-01");
+        
+        response.EnsureSuccessStatusCode();
+        var data = await response.Content.ReadFromJsonAsync<SWData>();
+        
+        Assert.NotNull(data);
+        Assert.Equal("Stockholm", data.City);
+        Assert.Equal(new DateOnly(2022, 01, 01), data.Date);
+    }
+    
+    [Fact]
+    public async Task GetData_AdminAuthorized_Test()
+    {
+        var app = new SolarWatchWebApplicationFactory();
+        var client = app.CreateClient();
+        
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("TestAdminScheme");
+        
+        var response = await client.GetAsync($"/api/SW/getdata/Stockholm/2022-01-01");
+        
+        response.EnsureSuccessStatusCode();
+        var data = await response.Content.ReadFromJsonAsync<SWData>();
+        
+        Assert.NotNull(data);
+        Assert.Equal("Stockholm", data.City);
+        Assert.Equal(new DateOnly(2022, 01, 01), data.Date);
+    }
+    
+    [Fact]
+    public async Task GetAll_AdminAuthorized_Test()
+    {
+        var app = new SolarWatchWebApplicationFactory();
+        var client = app.CreateClient();
+        
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("TestAdminScheme");
+        
+        var response = await client.GetAsync($"/api/SW/getAll");
+        
+        response.EnsureSuccessStatusCode();
+        var allData = await response.Content.ReadFromJsonAsync<List<SWData>>();
+        
+        Assert.NotNull(allData);
+        Assert.Empty(allData);
+    }
+    
+    [Fact]
+    public async Task Delete_AdminAuthorized_Test()
+    {
+        var app = new SolarWatchWebApplicationFactory();
+        var client = app.CreateClient();
+        
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("TestAdminScheme");
+        
+        var response = await client.DeleteAsync($"/api/SW/delete/1");
+        
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+    
+}
