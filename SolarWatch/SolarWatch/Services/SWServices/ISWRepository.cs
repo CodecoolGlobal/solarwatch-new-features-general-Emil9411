9561e64fcfa21043ba83bc4492791e226500e503
Emil9411 <almadi.emil94@gmail.com>
2024-04-01 15:02:20 +0200

    Interface for the SWRepository class updated.

--- SolarWatch/SolarWatch/Services/SWServices/ISWRepository.cs
+++ SolarWatch/SolarWatch/Services/SWServices/ISWRepository.cs
@@ -6,7 +6,8 @@ public interface ISWRepository
 {
     IEnumerable<SWData> GetAllSWDatas();
     SWData GetSWData(string city, DateOnly date);
+    Task<SWData> GetSWDataById(int id);
     void AddSWData(SWData swData);
-    void UpdateSWData(SWData swData);
-    void DeleteSWData(SWData swData);
+    Task UpdateSWData(SWData swData);
+    void DeleteSWData(int id);
 }
