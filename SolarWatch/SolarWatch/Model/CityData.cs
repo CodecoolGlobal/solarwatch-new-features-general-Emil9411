34fc114d1330b3341b791febefb71c4b5dd60fdc
Emil9411 <almadi.emil94@gmail.com>
2024-04-02 23:14:45 +0200

    New timezone and country properties added to the models.

--- SolarWatch/SolarWatch/Model/CityData.cs
+++ SolarWatch/SolarWatch/Model/CityData.cs
@@ -6,4 +6,6 @@ public class CityData
     public string? City { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
+    public string? TimeZone { get; set; }
+    public string? Country { get; set; }
 }
--- SolarWatch/SolarWatch/Model/SWData.cs
+++ SolarWatch/SolarWatch/Model/SWData.cs
@@ -1,9 +1,11 @@
 namespace SolarWatch.Model;
 
-public class SWData
+public class SwData
 {
     public int Id { get; set; }
     public string? City { get; set; }
+    public string? Country { get; set; }
+    public string? TimeZone { get; set; }
     public DateOnly Date { get; set; }
     public TimeOnly Sunrise { get; set; }
     public TimeOnly Sunset { get; set; }
@@ -15,12 +17,12 @@ public class SWData
             return false;
         }
 
-        var swData = (SWData)obj;
-        return City == swData.City && Date == swData.Date && Sunrise == swData.Sunrise && Sunset == swData.Sunset;
+        var swData = (SwData)obj;
+        return City == swData.City && Date == swData.Date && Sunrise == swData.Sunrise && Sunset == swData.Sunset && Country == swData.Country && TimeZone == swData.TimeZone;
     }
     
     public override int GetHashCode()
     {
-        return HashCode.Combine(City, Date, Sunrise, Sunset);
+        return HashCode.Combine(City, Date, Sunrise, Sunset, Country, TimeZone);
     }
 }
