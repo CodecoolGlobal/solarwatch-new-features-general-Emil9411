89489e08b95524351880951d8c7457e4748ce25b
Emil9411 <almadi.emil94@gmail.com>
2024-04-03 23:39:51 +0200

    Interface implementation for the new method.

--- SolarWatch/SolarWatch/Services/Auth/IAuthService.cs
+++ SolarWatch/SolarWatch/Services/Auth/IAuthService.cs
@@ -1,4 +1,5 @@
 using System.IdentityModel.Tokens.Jwt;
+using SolarWatch.Model;
 
 namespace SolarWatch.Services.Auth;
 
@@ -6,5 +7,6 @@ public interface IAuthService
 {
     Task<AuthResult> RegisterAsync(string email, string username, string password, string city, string role);
     Task<AuthResult> LoginAsync(string emailOrUserName, string password);
+    Task<ApplicationUser> GetUser(string email);
     JwtSecurityToken Verify(string token);
 }
