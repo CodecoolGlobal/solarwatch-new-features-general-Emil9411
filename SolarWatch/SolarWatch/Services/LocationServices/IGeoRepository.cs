177cfdff8b4b1101a89fc2afc22c25a3d4bccd2d
Emil9411 <almadi.emil94@gmail.com>
2024-04-03 23:41:22 +0200

    GetCityById method inserted in the interface.

--- SolarWatch/SolarWatch/Services/LocationServices/IGeoRepository.cs
+++ SolarWatch/SolarWatch/Services/LocationServices/IGeoRepository.cs
@@ -6,6 +6,7 @@ public interface IGeoRepository
 {
     IEnumerable<CityData> GetAllCities();
     CityData GetCity(string city);
+    Task<CityData> GetCityById(int id);
     void AddCity(CityData city);
     void UpdateCity(CityData city);
     void DeleteCity(CityData city);
