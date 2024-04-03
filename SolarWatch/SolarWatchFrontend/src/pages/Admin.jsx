dfb640e11020b0c285837166205671131e9942c9
Emil9411 <almadi.emil94@gmail.com>
2024-04-03 23:52:05 +0200

    New functions created, lot of refactoring.

--- SolarWatch/SolarWatchFrontend/src/pages/Admin.jsx
+++ SolarWatch/SolarWatchFrontend/src/pages/Admin.jsx
@@ -1,16 +1,91 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../design/index.css";
 import AdminTable from "../components/AdminTable";
-import AdminUpdateForm from "../components/AdminUpdateForm";
+import SolarWatchUpdateForm from "../components/SolarWatchUpdateForm";
+import CityDataUpdateForm from "../components/CityDataUpdateForm";
 
 function Admin() {
   const [cityData, setCityData] = useState([]);
-  const [cityToUpdate, setCityToUpdate] = useState({});
-  const [updateFrom, setUpdateFrom] = useState(false);
+  const [solarWatchData, setSolarWatchData] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
+  const [cityToUpdate, setCityToUpdate] = useState({});
+  const [solarWatchToUpdate, setSolarWatchToUpdate] = useState({});
+
+  const [cityUpdateForm, setCityUpdateForm] = useState(false);
+  const [solarWatchUpdateForm, setSolarWatchUpdateForm] = useState(false);
+
+  const [showSolarWatchTable, setShowSolarWatchTable] = useState(false);
+  const [showCityDataTable, setShowCityDataTable] = useState(false);
+
+  const [loadingScreen, setLoadingScreen] = useState(false);
+
+  // City data fucntions
+  async function fetchCityData() {
+    try {
+      setLoadingScreen(true);
+      const response = await fetch(`/api/Location/getall`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+      setCityData(data);
+      setLoadingScreen(false);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function handleCityDelete(id) {
+    try {
+      await fetch(`/api/Location/delete/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setCityData(cityData.filter((row) => row.id !== id));
+      console.log("Deleted");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function handleCityUpdate(id, data) {
+    try {
+      await fetch(`/api/Location/update/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      setCityData(cityData.map((row) => (row.id === id ? { ...row, ...data } : row)));
+      console.log("Updated");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  function handleCityDataToUpdate(data) {
+    setCityToUpdate(data);
+    setCityUpdateForm(true);
+    setShowCityDataTable(false);
+  }
+
+  function clearCityData() {
+    setCityToUpdate({});
+    setCityUpdateForm(false);
+    setShowCityDataTable(true);
+  }
+
+  // SolarWatch data functions
+  async function fetchSolarWatchData() {
+    try {
+      setLoadingScreen(true);
       const response = await fetch(`/api/SW/getall`, {
         method: "GET",
         headers: {
@@ -18,12 +93,15 @@ function Admin() {
         },
       });
       const data = await response.json();
-      setCityData(data);
+      console.log(data);
+      setSolarWatchData(data);
+      setLoadingScreen(false);
+    } catch (e) {
+      console.error(e);
     }
-    fetchData();
-  }, []);
+  }
 
-  async function handleDelete(id) {
+  async function handleSolarWatchDelete(id) {
     try {
       await fetch(`/api/SW/delete/${id}`, {
         method: "DELETE",
@@ -31,13 +109,14 @@ function Admin() {
           "Content-Type": "application/json",
         },
       });
-      setCityData(cityData.filter((row) => row.id !== id));
+      setSolarWatchData(solarWatchData.filter((row) => row.id !== id));
+      console.log("Deleted");
     } catch (e) {
       console.error(e);
     }
   }
 
-  async function handleUpdate(id, data) {
+  async function handleSolarWatchUpdate(id, data) {
     try {
       await fetch(`/api/SW/update/${id}`, {
         method: "PATCH",
@@ -46,39 +125,86 @@ function Admin() {
         },
         body: JSON.stringify(data),
       });
-      setCityData(
-        cityData.map((row) => (row.id === id ? { ...row, ...data } : row))
+      setSolarWatchData(
+        solarWatchData.map((row) => (row.id === id ? { ...row, ...data } : row))
       );
+      console.log("Updated");
     } catch (e) {
       console.error(e);
     }
   }
 
-  function handleDataToUpdate(data) {
-    setCityToUpdate(data);
-    setUpdateFrom(true);
+  function handleSolarWatchDataToUpdate(data) {
+    setSolarWatchToUpdate(data);
+    setSolarWatchUpdateForm(true);
+    setShowSolarWatchTable(false);
   }
 
-  function clearData() {
-    setCityToUpdate({});
-    setUpdateFrom(false);
+  function clearSolarWatchData() {
+    setSolarWatchToUpdate({});
+    setSolarWatchUpdateForm(false);
+    setShowSolarWatchTable(true);
   }
 
   return (
     <div>
       <h1>Admin site</h1>
       <br />
-      {cityData.length !== 0 && updateFrom === false ? (
-        <AdminTable data={cityData} onDelete={handleDelete} onUpdate={handleDataToUpdate} />
-      ) : updateFrom === true ? (
-        <AdminUpdateForm
+      <div>
+        <button
+          onClick={() => {
+            setShowCityDataTable(true);
+            setShowSolarWatchTable(false);
+            fetchCityData();
+          }}
+          disabled={showCityDataTable}
+        >
+          Show City Data
+        </button>
+        <button
+          onClick={() => {
+            setShowSolarWatchTable(true);
+            setShowCityDataTable(false);
+            fetchSolarWatchData();
+          }}
+          disabled={showSolarWatchTable}
+        >
+          Show SolarWatch Data
+        </button>
+      </div>
+      <br />
+      {showCityDataTable && !showSolarWatchTable && loadingScreen && !cityUpdateForm && !solarWatchUpdateForm ? (
+        <h2>Loading City Data...</h2>
+      ) : !showCityDataTable && showSolarWatchTable && loadingScreen && !cityUpdateForm && !solarWatchUpdateForm ? (
+        <h2>Loading SolarWatch Data...</h2>
+      ) : showCityDataTable && !showSolarWatchTable && !cityUpdateForm && !solarWatchUpdateForm ? (
+        <AdminTable
+          data={cityData}
+          handleDelete={handleCityDelete}
+          handleUpdate={handleCityDataToUpdate}
+        />
+      ) : !showCityDataTable && showSolarWatchTable && !cityUpdateForm && !solarWatchUpdateForm ? (
+        <AdminTable
+          data={solarWatchData}
+          handleDelete={handleSolarWatchDelete}
+          handleUpdate={handleSolarWatchDataToUpdate}
+        />
+      ) : cityUpdateForm && !solarWatchUpdateForm ? (
+        <CityDataUpdateForm
           data={cityToUpdate}
-          onUpdate={handleUpdate}
-          clearData={clearData}
+          handleUpdate={handleCityUpdate}
+          clearData={clearCityData}
+        />
+      ) : !cityUpdateForm && solarWatchUpdateForm ? (
+        <SolarWatchUpdateForm
+          data={solarWatchToUpdate}
+          handleUpdate={handleSolarWatchUpdate}
+          clearData={clearSolarWatchData}
         />
       ) : (
-        <h2>No data available</h2>
+        <h2>Waiting for instructions...</h2>
       )}
+
     </div>
   );
 }
