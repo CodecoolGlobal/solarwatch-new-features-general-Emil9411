599db157f0b618f8e9bdc287c43a16d62a7af648
Emil9411 <almadi.emil94@gmail.com>
2024-04-03 11:45:57 +0200

    Json processor for solarwatch refactored.

--- SolarWatch/SolarWatch/Services/SWServices/JsonProcessorSW.cs
+++ SolarWatch/SolarWatch/Services/SWServices/JsonProcessorSW.cs
@@ -6,32 +6,45 @@ namespace SolarWatch.Services.SWServices;
 public class JsonProcessorSw : IJsonProcessorSw
 {
     private static readonly string[] InputFormats = { "h:mm:ss tt", "hh:mm:ss tt" };
-    private const string OutputFormat = "HH:mm:ss";
 
     public TimeOnly[] SolarJsonProcessor(string data)
     {
         TimeOnly returnSunrise = new();
         TimeOnly returnSunset = new();
 
-        var json = JsonDocument.Parse(data);
-
-        var sunrise = json.RootElement.GetProperty("results").GetProperty("sunrise");
-        var sunset = json.RootElement.GetProperty("results").GetProperty("sunset");
-
-        var sunriseString = sunrise.GetString();
-        var sunsetString = sunset.GetString();
-
-        foreach (var format in InputFormats)
+        try
         {
-            if (DateTime.TryParseExact(sunriseString, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime sunriseTime))
-            {
-                returnSunrise = new TimeOnly(sunriseTime.Hour, sunriseTime.Minute, sunriseTime.Second);
-            }
+            var json = JsonDocument.Parse(data);
 
-            if (DateTime.TryParseExact(sunsetString, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime sunsetTime))
+            var rootResults = json.RootElement.GetProperty("results");
+            var sunrise = rootResults.GetProperty("sunrise");
+            var sunset = rootResults.GetProperty("sunset");
+
+            var sunriseString = sunrise.GetString();
+            var sunsetString = sunset.GetString();
+
+            foreach (var format in InputFormats)
             {
-                returnSunset = new TimeOnly(sunsetTime.Hour, sunsetTime.Minute, sunsetTime.Second);
+                if (DateTime.TryParseExact(sunriseString, format, CultureInfo.InvariantCulture, DateTimeStyles.None,
+                        out DateTime sunriseTime))
+                {
+                    returnSunrise = new TimeOnly(sunriseTime.Hour, sunriseTime.Minute, sunriseTime.Second);
+                }
+
+                if (DateTime.TryParseExact(sunsetString, format, CultureInfo.InvariantCulture, DateTimeStyles.None,
+                        out DateTime sunsetTime))
+                {
+                    returnSunset = new TimeOnly(sunsetTime.Hour, sunsetTime.Minute, sunsetTime.Second);
+                }
             }
+        }
+        catch (JsonException)
+        {
+            return new TimeOnly[] { };
+        }
+        catch (KeyNotFoundException)
+        {
+            return new TimeOnly[] { };
         }
 
         return new[] { returnSunrise, returnSunset };
