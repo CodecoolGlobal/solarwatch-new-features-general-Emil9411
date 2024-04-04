fd1396eda0be9582f2a69ca375ea211b544f6269
Emil9411 <almadi.emil94@gmail.com>
2024-04-04 11:07:19 +0200

    Logout button now uses modal.

--- SolarWatch/SolarWatchFrontend/src/components/LogoutButton.jsx
+++ SolarWatch/SolarWatchFrontend/src/components/LogoutButton.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import MessageModal from "./MessageModal";
 
 function LogoutButton() {
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const [showErrorModal, setShowErrorModal] = useState(false);
+
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -14,20 +18,33 @@ function LogoutButton() {
           "Content-Type": "application/json",
         },
       });
-      if (response.ok) {
-        if (location.pathname !== "/") {
-          navigate("/");
-          window.location.reload();
-        } else {
-          window.location.reload();
-        }
+      if (!response.ok) {
+        throw new Error("Logout failed");
+      } else {
+        setShowSuccessModal(true);
       }
     } catch (error) {
+      setShowErrorModal(true);
       console.error("Logout failed", error);
     }
   }
 
-  return <button onClick={handleLogout}>Logout</button>;
+  function handleCloseSuccessModal() {
+    setShowSuccessModal(false);
+    location.pathname === "/" ? window.location.reload() : navigate("/");
+  }
+
+  function handleCloseErrorModal() {
+    setShowErrorModal(false);
+  }
+
+  return (
+    <>
+      <button onClick={handleLogout}>Logout</button>
+      <MessageModal isOpen={showSuccessModal} message="Logout successful" handleClose={handleCloseSuccessModal} />
+      <MessageModal isOpen={showErrorModal} message="Logout failed. Please try again." handleClose={handleCloseErrorModal} />
+    </>
+  );
 }
 
 export default LogoutButton;
